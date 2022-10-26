180a3047a848d4353ef07ff7587ac0c3eecb71fe
szuryanailham <szuryanailham090102@gmail.com>
2022-10-27 05:58:35 +0700

    ini adalah website paket wisata dan rental mobil

---
+++ script2.js
@@ -0,0 +1,7 @@
+// efek sideBar
+const sidenav = document.querySelectorAll(".sidenav");
+M.Sidenav.init(sidenav);
+// efek slider
+
+const parallax = document.querySelectorAll(".parallax");
+M.Parallax.init(parallax);
